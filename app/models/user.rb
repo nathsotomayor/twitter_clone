1cc2986990c89089aac8c870469295c9f5f9024d
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-03-09 17:22:25 -0500

    Create Tweet model and controller, also made some frontend styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :social_profiles, dependent: :delete_all
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_010217) do
+ActiveRecord::Schema.define(version: 2021_03_09_213007) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "tweet"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
@@ -27,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_03_09_010217) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
