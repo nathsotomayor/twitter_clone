ed63b7ebf2b3324e438aea14f413adcc6a4ff46a
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-03-10 17:21:02 -0500

    Rendering succesfully form in feed and then in index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,64 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: %i[ show edit update destroy ]
+  #before_action :authenticate_user!
+
+
+  def index
+    @tweets = Tweet.all
+    @tweet = Tweet.new
+  end
+
+  def show
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def edit
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: "Tweet was successfully created." }
+        format.json { render :show, status: :created, location: @tweet }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html { redirect_to @tweet, notice: "Tweet was successfully updated." }
+        format.json { render :show, status: :ok, location: @tweet }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    respond_to do |format|
+      format.html { redirect_to tweet_url, notice: "Tweet was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+
+    def tweet_params
+      params.require(:tweet).permit(:tweet)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :social_profiles, dependent: :delete_all
+  has_many :tweets, dependent: :delete_all
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
