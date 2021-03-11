a3467851c4a480408aeea11a02e043c7a52f962f
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-03-11 15:37:04 -0500

    Adding associations in User and Relatioship models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,28 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :tweets, dependent: :delete_all
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :delete_all
+
+  has_many :following, through: :active_relationships, source: followed
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
+
+
+  def follow(other)
+    active_relationships.create(followed_id: other_id)
+  end
+
+  def unfollow(other)
+    active_relationships.find_by(followed_id: other_id).destroy
+  end
+
+  def following?(other)
+    following.include?(other)
+  end
+
+
 end
