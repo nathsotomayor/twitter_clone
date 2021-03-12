fb81ae4db818b23cc2ad2429412188dec5e8985c
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-03-11 21:53:46 -0500

    Button follow/unfollow functionality fixed - All working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :tweets, dependent: :delete_all
-  
+
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :delete_all
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :delete_all
 
@@ -15,18 +15,16 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
 
-
   def follow(other)
-    active_relationships.create(followed_id: other_id)
+    active_relationships.create(followed_id: other.id)
   end
 
   def unfollow(other)
-    active_relationships.find_by(followed_id: other_id).destroy
+    active_relationships.find_by(followed_id: other.id).destroy
   end
 
   def following?(other)
     following.include?(other)
   end
-
 
 end
