017260354e10c646605d4549f78010230b161509
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-03-26 14:58:54 -0500

    Pagination to profiles tweets, button to follow in profiles, list of following users (All Working)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tweets = @user.tweets
+    @tweets = @user.tweets.paginate(page: params[:page], per_page: 10)
   end
 
   def following
